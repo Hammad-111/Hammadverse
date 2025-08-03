b3af8d1efe440e04e0e9339f9e2ca4c41e459b21
Hammad Javed <96216930+Hammad-111@users.noreply.github.com>
2025-08-03 13:57:23 +0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         </div>
 
         <div className="about-details">
-          <h2>Bahlu Mera or Zubair ka yar han Thanks</h2>
+          <h2>Hammad Javed</h2>
           <h4>Full Stack Developer | MERN | React Native | .NET | Python </h4>
           <p className="summary">
              I am a passionate and dedicated programmer with hands-on experience in web and mobile app development. 
